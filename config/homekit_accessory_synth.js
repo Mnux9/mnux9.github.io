54ed0d93b2a0dce5ad72e4d492614ea8d371a2ae
mnux <MY_NAME@example.com>
2025-02-08 10:55:29 +0100

    add modules page

--- config/homekit_accessory_synth.js
+++ config/homekit_accessory_synth.js
@@ -11,7 +11,8 @@ let hubAccessory = "HOMEKIT_ACCESSORY(.id=1, .category=homekit_accessory_categor
             HOMEKIT_CHARACTERISTIC(NAME, \"mnux.xyz hub i1\"),\
             HOMEKIT_CHARACTERISTIC(MANUFACTURER, \"mnux.xyz\"),\
             HOMEKIT_CHARACTERISTIC(SERIAL_NUMBER, \"0001\"),\
-            HOMEKIT_CHARACTERISTIC(MODEL, \"i1revA\"),\
+            HOMEKIT_CHARACTERISTIC(MODEL, \"i1\"),\
+            HOMEKIT_CHARACTERISTIC(HARDWARE_REVISION, \"A\"),\
             HOMEKIT_CHARACTERISTIC(FIRMWARE_REVISION, \"0.1\"),\
             HOMEKIT_CHARACTERISTIC(IDENTIFY, my_accessory_identify),\
             NULL\
@@ -43,13 +44,14 @@ let switchAccessory = "HOMEKIT_ACCESSORY(.id=4, .category=homekit_accessory_cate
         }),\
         HOMEKIT_SERVICE(SWITCH, .primary=true, .characteristics=(homekit_characteristic_t*[]) {\
             HOMEKIT_CHARACTERISTIC(NAME, \"Switch\"),\
-            &cha_switch_on,\
+            HOMEKIT_CHARACTERISTIC(ON, my_accessory_identify),\
             NULL\
         }),\
         NULL\
         }),"
 
 
+let id = 3;
 
 
 // this variable is synthesized like this : hubAccessory + all other accessories + termination
@@ -57,5 +59,88 @@ let switchAccessory = "HOMEKIT_ACCESSORY(.id=4, .category=homekit_accessory_cate
 function synthesize(){
     textBox.innerHTML = ("connected");
     //var x = "hi";
-    document.getElementById("textBox").value = (hubAccessory + thermometerAccessory + thermometerAccessory+switchAccessory+"NULL");
+    //document.getElementById("textBox").value = (hubAccessory + thermometerAccessory + thermometerAccessory+switchAccessory+"NULL");
+    //addSwitchAccessory(1,"testSW",1);
+    document.getElementById("textBox").value = (hubAccessory+addLightAccessory(2,"dimbulb",0)+addTemperatureAccessory(3,"aht20")+addHumidityAccessory(4,"aht20")+"NULL");
+
 }   
+
+
+
+
+function addLightAccessory(id, name, dimmable){
+    characteristics = "";
+    
+    if (dimmable){
+        characteristics = "&cha_bright,";
+    }
+    else{
+        characteristics= "";
+    }
+    
+    //id = id + 1;
+    //document.getElementById("textBox").value = (buffer);
+    return "HOMEKIT_ACCESSORY(.id="+id+", .category=homekit_accessory_category_lightbulb, .services=(homekit_service_t*[]) {\
+        HOMEKIT_SERVICE(ACCESSORY_INFORMATION, .characteristics=(homekit_characteristic_t*[]) {\
+            HOMEKIT_CHARACTERISTIC(NAME, \""+name+"\"),\
+            HOMEKIT_CHARACTERISTIC(IDENTIFY, my_accessory_identify),\
+            NULL\
+        }),\
+        HOMEKIT_SERVICE(LIGHTBULB, .primary=true, .characteristics=(homekit_characteristic_t*[]) {\
+            &cha_switch_on," +characteristics+"\
+            NULL\
+        }),\
+        NULL\
+        }),";
+
+}
+
+
+function addSwitchAccessory(id, name){
+    //document.getElementById("textBox").value = (buffer);
+    return "HOMEKIT_ACCESSORY(.id="+id+", .category=homekit_accessory_category_switch, .services=(homekit_service_t*[]) {\
+        HOMEKIT_SERVICE(ACCESSORY_INFORMATION, .characteristics=(homekit_characteristic_t*[]) {\
+            HOMEKIT_CHARACTERISTIC(NAME, \""+name+"\"),\
+            HOMEKIT_CHARACTERISTIC(IDENTIFY, my_accessory_identify),\
+            NULL\
+        }),\
+        HOMEKIT_SERVICE(SWITCH, .primary=true, .characteristics=(homekit_characteristic_t*[]) {\
+            &cha_switch_on,\
+            NULL\
+        }),\
+        NULL\
+        }),";
+}
+
+
+function addTemperatureAccessory(id, name){
+    //document.getElementById("textBox").value = (buffer);
+    return "HOMEKIT_ACCESSORY(.id="+id+", .category=homekit_accessory_category_sensor, .services=(homekit_service_t*[]) {\
+        HOMEKIT_SERVICE(ACCESSORY_INFORMATION, .characteristics=(homekit_characteristic_t*[]) {\
+            HOMEKIT_CHARACTERISTIC(NAME, \""+name+"\"),\
+            HOMEKIT_CHARACTERISTIC(IDENTIFY, my_accessory_identify),\
+            NULL\
+        }),\
+        HOMEKIT_SERVICE(TEMPERATURE_SENSOR, .primary=true, .characteristics=(homekit_characteristic_t*[]) {\
+            &cha_temperature,\
+            NULL\
+        }),\
+        NULL\
+        }),"
+}
+
+function addHumidityAccessory(id, name){
+    //document.getElementById("textBox").value = (buffer);
+    return "HOMEKIT_ACCESSORY(.id="+id+", .category=homekit_accessory_category_sensor, .services=(homekit_service_t*[]) {\
+    	HOMEKIT_SERVICE(ACCESSORY_INFORMATION, .characteristics=(homekit_characteristic_t*[]) {\
+			HOMEKIT_CHARACTERISTIC(NAME, \""+name+"\"),\
+			HOMEKIT_CHARACTERISTIC(IDENTIFY, my_accessory_identify),\
+			NULL\
+		}),\
+    	HOMEKIT_SERVICE(HUMIDITY_SENSOR, .primary=true, .characteristics=(homekit_characteristic_t*[]) {\
+			&cha_humidity,\
+			NULL\
+		}),\
+		NULL\
+	}),"
+}
