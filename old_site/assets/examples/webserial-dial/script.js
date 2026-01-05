b46cbf5cb738e6db93d8075448ce51d16d3b23ad
scts <56221832+Mnux9@users.noreply.github.com>
2026-01-05 13:43:44 +0100

    delete everythinh

--- old_site/assets/examples/webserial-dial/script.js
+++
@@ -1,9 +0,0 @@
-setInterval(function() {
-    var newVal = Math.floor((Math.random() * 179) + 1);
-
-    $('.gauge--3 .semi-circle--mask').attr({
-        style: '-webkit-transform: rotate(' + newVal + 'deg);' +
-        '-moz-transform: rotate(' + newVal + 'deg);' +
-        'transform: rotate(' + newVal + 'deg);'
-    });
-}, 1000);
